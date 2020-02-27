00b72ad7362630523057f27dbf673cd9afc8dc95
Ashwathi Shiva <ashwathi.shiva@qlik.com>
2020-02-27 08:07:40 -0500

    minor refactoring to switch to k8s secret

--- pkg/config/k8s_secret.go
+++ pkg/config/k8s_secret.go
@@ -1,18 +1,22 @@
 package config
 
-const (
-	k8sSecretAPIVersion = "v1"
-	k8sSecretKind       = "Secret"
-	k8sSecretType       = "Opaque"
+import (
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // ConstructK8sSecretStructure constructs a K8s Secret struct
-func (k8sSecret *K8sSecret) ConstructK8sSecretStructure(secretName string, dataMap map[string]string) {
-	if k8sSecret != nil {
-		k8sSecret.ApiVersion = k8sSecretAPIVersion
-		k8sSecret.Kind = k8sSecretKind
-		k8sSecret.Name = secretName
-		k8sSecret.Type = k8sSecretType
-		k8sSecret.Data = dataMap
+func ConstructK8sSecretStructure(secretName string, dataMap map[string][]byte) *v1.Secret {
+	return &v1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Secret",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: secretName,
+			// Namespace: namespace,
+		},
+		Type: v1.SecretTypeOpaque,
+		Data: dataMap,
 	}
 }
