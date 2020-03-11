1f0122abfbab7f9f9be09dba1cba260f87352191
Sanat Nayar <fqg@qlik.com>
2020-03-11 15:47:14 -0400

    added GitOps to spec

--- pkg/config/type.go
+++ pkg/config/type.go
@@ -78,6 +78,6 @@ type Repo struct {
 type Ops struct {
 	Enabled     string `json:"enabled,omitempty" yaml:"enabled,omitempty"`
 	Schedule    string `json:"schedule,omitempty" yaml:"schedule,omitempty"`
-	WatchBranch string `json:"watchBranch,omitempty" yaml:"schedule,omitempty"`
+	WatchBranch string `json:"watchBranch,omitempty" yaml:"watchBranch,omitempty"`
 	Image       string `json:"image,omitempty" yaml:"image,omitempty"`
 }
